805dfaae1022afce02c32fcd36eafdff24c3283e
Emmanuel Adu Gyamfi <emm7494@gmail.com>
2020-06-03 00:08:17 +0000

    Added app Header & Footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'product-listing', pathMatch: 'full' },
+  { path: 'product-listing', component: ProductListingComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListingComponent,
+    ShoppingCartComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
