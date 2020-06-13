121e4b10d8f1d232192ce8e970c81795446e71b2
Emmanuel Adu Gyamfi <emm7494@gmail.com>
2020-06-13 16:14:43 +0000

    Re-sourced card images from local assets folder.

--- src/app/product-listing/product-listing-list/product-listing-list.component.ts
+++ src/app/product-listing/product-listing-list/product-listing-list.component.ts
@@ -8,19 +8,33 @@ import { Product } from '../../shared/models/product.model';
 })
 export class ProductListingListComponent implements OnInit {
   products: Product[] = [
-    new Product('Nokia', 499.99, 'https://source.unsplash.com/random/1'),
-    new Product('Apple', 999.99, 'https://source.unsplash.com/random/2'),
-    new Product('OnePlus', 299.99, 'https://source.unsplash.com/random/3'),
-    new Product('LG', 199.99, 'https://source.unsplash.com/random/4'),
-    new Product('Samsung', 599.99, 'https://source.unsplash.com/random/5'),
-    new Product('Huawei', 399.99, 'https://source.unsplash.com/random/6'),
-    new Product('Motorola', 499.99, 'https://source.unsplash.com/random/7'),
-    new Product('Oppo', 999.99, 'https://source.unsplash.com/random/8'),
-    new Product('Infinix', 299.99, 'https://source.unsplash.com/random/9'),
-    new Product('Nexus', 199.99, 'https://source.unsplash.com/random/10'),
-    new Product('Blackberry', 599.99, 'https://source.unsplash.com/random/11'),
-    new Product('Sony', 399.99, 'https://source.unsplash.com/random/12'),
+    new Product('ONEPLUS', 299.99, 'assets/phone-images/oneplus.jpg'),
+    new Product('HUAWEI', 399.99, 'assets/phone-images/huawei.jpg'),
+    new Product('APPLE', 999.99, 'assets/phone-images/apple.jpg'),
+    new Product('MOTOROLA', 499.99, 'assets/phone-images/motorola.jpg'),
+    new Product('LG', 199.99, 'assets/phone-images/lg.jpg'),
+    new Product('OPPO', 999.99, 'assets/phone-images/oppo.jpg'),
+    new Product('INFINIX', 299.99, 'assets/phone-images/infinix.jpg'),
+    new Product('NEXUS', 199.99, 'assets/phone-images/nexus.jpg'),
+    new Product('NOKIA', 499.99, 'assets/phone-images/nokia.jpg'),
+    new Product('BLACKBERRY', 599.99, 'assets/phone-images/blackberry.jpg'),
+    new Product('SAMSUNG', 599.99, 'assets/phone-images/samsung.jpg'),
+    new Product('SONY', 399.99, 'assets/phone-images/sony.jpg'),
   ];
+  // products: Product[] = [
+  //   new Product('NOKIA', 499.99, 'https://source.unsplash.com/random/1'),
+  //   new Product('APPLE', 999.99, 'https://source.unsplash.com/random/2'),
+  //   new Product('ONEPLUS', 299.99, 'https://source.unsplash.com/random/3'),
+  //   new Product('LG', 199.99, 'https://source.unsplash.com/random/4'),
+  //   new Product('SAMSUNG', 599.99, 'https://source.unsplash.com/random/5'),
+  //   new Product('HUAWEI', 399.99, 'https://source.unsplash.com/random/6'),
+  //   new Product('MOTOROLA', 499.99, 'https://source.unsplash.com/random/7'),
+  //   new Product('OPPO', 999.99, 'https://source.unsplash.com/random/8'),
+  //   new Product('INFINIX', 299.99, 'https://source.unsplash.com/random/9'),
+  //   new Product('NEXUS', 199.99, 'https://source.unsplash.com/random/10'),
+  //   new Product('BLACKBERRY', 599.99, 'https://source.unsplash.com/random/11'),
+  //   new Product('SONY', 399.99, 'https://source.unsplash.com/random/12'),
+  // ];
   constructor() {}
 
   ngOnInit(): void {}
