1c435aab3bf956dbe9f2106da726e508840daff8
Emmanuel Adu Gyamfi <emm7494@gmail.com>
2020-06-14 18:13:20 +0000

    Added card hover animation.

--- src/app/product-listing/product-listing-list/product-listing-list.component.ts
+++ src/app/product-listing/product-listing-list/product-listing-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product.model';
-
+import { faCartPlus, faHeart } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-product-listing-list',
   templateUrl: './product-listing-list.component.html',
@@ -8,18 +8,18 @@ import { Product } from '../../shared/models/product.model';
 })
 export class ProductListingListComponent implements OnInit {
   products: Product[] = [
-    new Product('ONEPLUS', 299.99, 'assets/phone-images/oneplus.jpg'),
-    new Product('HUAWEI', 399.99, 'assets/phone-images/huawei.jpg'),
-    new Product('APPLE', 999.99, 'assets/phone-images/apple.jpg'),
-    new Product('MOTOROLA', 499.99, 'assets/phone-images/motorola.jpg'),
-    new Product('LG', 199.99, 'assets/phone-images/lg.jpg'),
-    new Product('OPPO', 999.99, 'assets/phone-images/oppo.jpg'),
-    new Product('INFINIX', 299.99, 'assets/phone-images/infinix.jpg'),
-    new Product('NEXUS', 199.99, 'assets/phone-images/nexus.jpg'),
-    new Product('NOKIA', 499.99, 'assets/phone-images/nokia.jpg'),
-    new Product('BLACKBERRY', 599.99, 'assets/phone-images/blackberry.jpg'),
-    new Product('SAMSUNG', 599.99, 'assets/phone-images/samsung.jpg'),
-    new Product('SONY', 399.99, 'assets/phone-images/sony.jpg'),
+    new Product('ONEPLUS', 299.99, 'assets/img/phones/oneplus.jpg'),
+    new Product('HUAWEI', 399.99, 'assets/img/phones/huawei.jpg'),
+    new Product('APPLE', 999.99, 'assets/img/phones/apple.jpg'),
+    new Product('MOTOROLA', 499.99, 'assets/img/phones/motorola.jpg'),
+    new Product('LG', 199.99, 'assets/img/phones/lg.jpg'),
+    new Product('OPPO', 999.99, 'assets/img/phones/oppo.jpg'),
+    new Product('INFINIX', 299.99, 'assets/img/phones/infinix.jpg'),
+    new Product('NEXUS', 199.99, 'assets/img/phones/nexus.jpg'),
+    new Product('NOKIA', 499.99, 'assets/img/phones/nokia.jpg'),
+    new Product('BLACKBERRY', 599.99, 'assets/img/phones/blackberry.jpg'),
+    new Product('SAMSUNG', 599.99, 'assets/img/phones/samsung.jpg'),
+    new Product('SONY', 399.99, 'assets/img/phones/sony.jpg'),
   ];
   // products: Product[] = [
   //   new Product('NOKIA', 499.99, 'https://source.unsplash.com/random/1'),
@@ -35,6 +35,8 @@ export class ProductListingListComponent implements OnInit {
   //   new Product('BLACKBERRY', 599.99, 'https://source.unsplash.com/random/11'),
   //   new Product('SONY', 399.99, 'https://source.unsplash.com/random/12'),
   // ];
+  faCartPlus = faCartPlus;
+  faHeart = faHeart;
   constructor() {}
 
   ngOnInit(): void {}
