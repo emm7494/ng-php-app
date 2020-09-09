df5a9eba4967985deb98a63ba181bfdd0e0955aa
Emmanuel Adu Gyamfi <emm7494@gmail.com>
2020-09-09 04:20:23 +0000

    Minor changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'ng-php-app';
   // loginModalVisible = true;
   loginModalVisible = false;
   ngOnInit() {
-    console.log('app');
+    
   }
 
   showLoginModal(status: boolean) {
